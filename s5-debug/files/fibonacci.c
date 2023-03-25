a4c82e31c45bae7a7f3e4d0e1a091e498d9da00a
Biel Altimira <biel.altimira@estudiantat.upc.edu>
2023-03-25 16:19:42 +0100

    sessió 5 debug acabada

--- s5-debug/files/fibonacci.c
+++
@@ -1,7 +0,0 @@
-// https://ca.wikipedia.org/wiki/Leonardo_de_Pisa
-
-unsigned long fibonacci(unsigned long n)
-{
-   if ((n == 0) || (n == 1)) return n;
-   return fibonacci(n-1) + fibonacci(n-2);
-}
